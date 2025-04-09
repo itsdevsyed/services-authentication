bf82d875b4b956dbc4850ccae877d0fa5b4ecd0f
syed42u <syedshoib25@gmail.com>
2025-04-09 20:38:00 +0530

    playwint with jest and supertest ok

--- tests/auth/profile.test.js
+++ tests/auth/profile.test.js
@@ -1,1 +1,8 @@
 import jest from 'jest'
+
+
+descibe('Get /api/auth/profile' , () => {
+    it('should return user profile', async () => {
+        const res = await request(app).get('/api/auth/profile')
+    })
+})
