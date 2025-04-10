ebf0954a2179a0e688d49af856cb2df99f414450
syed42u <syedshoib25@gmail.com>
2025-04-10 17:06:47 +0530

    eracecs the conflict

--- tests/auth/profile.test.js
+++ tests/auth/profile.test.js
@@ -1,20 +1,12 @@
 import jest from 'jest'
-<<<<<<< HEAD
 import app from '../../src/app.js'
 import supertest from 'supertest'
-=======
->>>>>>> bf82d875b4b956dbc4850ccae877d0fa5b4ecd0f
 
 
 descibe('Get /api/auth/profile' , () => {
     it('should return user profile', async () => {
         const res = await request(app).get('/api/auth/profile')
-<<<<<<< HEAD
         cos
     })
 })
 
-=======
-    })
-})
->>>>>>> bf82d875b4b956dbc4850ccae877d0fa5b4ecd0f
