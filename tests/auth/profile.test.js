36c5f8f2a03cc7baa8d1a45909f08e99661e0d24
syed42u <syedshoib25@gmail.com>
2025-04-10 13:24:40 +0530

    minimal profile test

--- tests/auth/profile.test.js
+++ tests/auth/profile.test.js
@@ -1,20 +1,13 @@
 import jest from 'jest'
-<<<<<<< HEAD
 import app from '../../src/app.js'
 import supertest from 'supertest'
-=======
->>>>>>> bf82d875b4b956dbc4850ccae877d0fa5b4ecd0f
 
 
 descibe('Get /api/auth/profile' , () => {
     it('should return user profile', async () => {
         const res = await request(app).get('/api/auth/profile')
-<<<<<<< HEAD
+ HEAD
         cos
     })
 })
 
-=======
-    })
-})
->>>>>>> bf82d875b4b956dbc4850ccae877d0fa5b4ecd0f
