8beb291ebbd6ff48635bb4f3da2036693b7d5f20
Syed Shoaib <165158831+itsdevsyed@users.noreply.github.com>
2025-04-10 22:18:20 +0530

    added the refrest.test.js logic to work on the testing for refresh token as well

---
+++ tests/auth/refresh.test.js
@@ -0,0 +1,24 @@
+import jest from 'jest'
+import app from '../../src/app.js'
+import supertest from 'supertest'
+
+const request = supertest(app)
+
+descibe('Get /api/auth/profile' , () => {
+    it('should return user profile', async () => {
+        const res = await request.get('/api/auth/profile')
+        const res2 = await request.get('/api/auth/profile')
+        expect(res2.status).toBe(200)
+    })
+})
+
+descibe('Get /api/auth/profile' , () => {
+    it('should return user profile', async () => {
+        const res = await request(app).get('/api/auth/profile')
+        const res2 = await request(app).get('/api/auth/profile')
+        expect(res2.status).toBe(200)
+    })
+})
+
+
+
