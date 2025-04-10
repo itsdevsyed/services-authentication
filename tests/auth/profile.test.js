d00b7a1a85fde05949cf709d1c1db4094da1a555
Syed Shoaib <165158831+itsdevsyed@users.noreply.github.com>
2025-04-10 21:49:16 +0530

    made some changes in the profiel.test.js for better use

--- tests/auth/profile.test.js
+++ tests/auth/profile.test.js
@@ -1,4 +1,6 @@
 import jest from 'jest'
+import app from '../../src/app.js'
+import supertest from 'supertest'
 
 const request = supertest(app)
 
@@ -13,7 +15,10 @@ descibe('Get /api/auth/profile' , () => {
 descibe('Get /api/auth/profile' , () => {
     it('should return user profile', async () => {
         const res = await request(app).get('/api/auth/profile')
+        const res2 = await request(app).get('/api/auth/profile')
+        expect(res2.status).toBe(200)
     })
 })
 
 g
+
