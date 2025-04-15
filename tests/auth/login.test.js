1a45f7266c464e8792488574a686ea49f3fa97b3
itsdevsyed <shoaib.syed4@outlook.com>
2025-04-15 14:20:43 +0530

    added the ths tuff what i dont knwo but somehowe its working

--- tests/auth/login.test.js
+++ tests/auth/login.test.js
@@ -1,32 +1,35 @@
 import request from 'supertest'
 import app from '../../src/app.js'
-import { JsonWebTokenError } from 'jsonwebtoken'
+import sequelize from '../../src/config/database.js'
 
-import jest from 'jest'
-import app from '../../src/app.js'
-import supertest from 'supertest'
+const userData = {
+    email: 'test'+Date.now()+'@example.com',
+    password: 'strongpassword',
+};
+beforeAll(async () => {
+    await sequelize.sync();
+    await request(app).post('/api/auth/register').send(userData);
+});
 
-const request = supertest(app)
+afterAll(async () => {
+    await sequelize.drop();
+});
 
-descibe('Get /api/auth/profile' , () => {
-    it('should return user profile', async () => {
-        const res = await request.get('/api/auth/profile')
-        const res2 = await request.get('/api/auth/profile')
-        expect(res2.status).toBe(200)
+descibe('Post /api/auth/login' , () => {
+    it('should login a user successfully', async()=> {
+        const res = await request(app).post('/api/auth/login') .send({
+            email:'test${date.now()}@example.com ',
+            pasword:'strongpassword',
+        });
+        expect(res.statusCode).tobe(201);
+        expect(res.body.success).tobe(true);
+        expect(res.body.data).toHaveProperty('user');
+        expect(res.body.data).toHaveProperty('token');
+
+    });
+    it('should return error for missing field', async () => {
+        const res = await request(app).post('/api/auth/registration')
     })
-})
-
-descibe('Get /api/auth/profile' , () => {
-    it('should return user profile', async () => {
-        const res = await request(app).get('/api/auth/profile')
-        const res2 = await request(app).get('/api/auth/profile')
-        expect(res2.status).toBe(200)
-    })
-})
-
-
-
-
-
+} )
 
 
